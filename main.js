f298306dd58881ee00388ba44355d5464729e4e5
Julio <juliofreitas.bm@gmail.com>
2020-05-30 19:18:12 -0300

    Conceitos - Let e const

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-class Matematica {
-	static soma(a, b) {
-		return a + b;
+function teste(x) {
+	var y = 2;
+
+	if(x > 5) {
+		var y = 4;
+		console.log(x, y);
 	}
 }
 
-console.log(Matematica.soma(1, 2))
+teste(10);
