9cb2ddbd20c67d768ec3a1e551b82eb3b1f8842c
Julio <juliofreitas.bm@gmail.com>
2020-05-30 19:36:50 -0300

    Conceitos - Operações em Array

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-function teste(x) {
-	var y = 2;
+const arr = [1, 3, 4, 5, 8, 9]
 
-	if(x > 5) {
-		var y = 4;
-		console.log(x, y);
-	}
-}
+const newArr = arr.map(function(item, index) {
+	return item*2;
+});
 
-teste(10);
+console.log(arr);
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next) {
+	return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item) {
+	return item % 2 == 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item) {
+	return item === 4;
+});
+
+console.log(find)
