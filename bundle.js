e86c210818935b8c735423ada584a6b47479f347
Julio <juliofreitas.bm@gmail.com>
2020-05-30 23:43:50 -0300

    Conceitos - Object Short Syntax

--- bundle.js
+++ bundle.js
@@ -2,5 +2,9 @@
 
 var nome = "Diego";
 var idade = 23;
-console.log('Meu nome é ' + nome + ' e eu tenho ' + idade + ' anos.');
-console.log("Meu nome \xE9 ".concat(nome, " e eu tenho ").concat(idade, " anos."));
+var usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: 'Rocketseat'
+};
+console.log(usuario);
--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const nome = "Diego";
 const idade = 23;
 
-console.log('Meu nome é ' + nome + ' e eu tenho ' + idade + ' anos.' )
-console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`)
+const usuario = {
+	nome,
+	idade,
+	empresa: 'Rocketseat',
+}
+
+console.log(usuario);
+
