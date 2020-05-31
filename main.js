1f2786c051dc0853285aa2632d3b1c18825351f8
Julio <juliofreitas.bm@gmail.com>
2020-05-30 23:26:23 -0300

    Conceitos - Operadores rest/spread

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
+//REST
+
 const usuario = {
 	nome: 'Julio',
 	idade: 23,
-	endereco: {
-		cidade: 'Goiânia',
-		estado: 'SC',
-	},
+	empresa: 'Rocketseat',
 };
 
-function mostraNome({nome, idade}) {
-	console.log(nome, idade);
+const {nome, ...resto} = usuario;
+
+console.log(nome);
+console.log(resto)
+
+const arr = [1, 2, 3, 4];
+const [a, b, ...c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma(...params) {
+	return params.reduce((total, next) => total + next);
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4, 6));
+
+//SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [ ...arr1, ...arr2 ];
+
+console.log(arr3);
+
+const usuario1 = {
+	nome: 'Diego',
+	idade: 23,
+	empresa: 'Rocketseat',
+};
+
+const usuario2 = { ...usuario1, nome: 'Gabriel'};
+
+console.log(usuario2);
